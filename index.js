672521a26145396e90feeb7e5c000723ff3e0437
LiviuDragutan <liviu_dragutan@yahoo.com>
2023-09-22 09:07:50 +0300

    newcommit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+let number = 10;
+let string = 'a string';
+let boolean = false;
+let undef = undefined;
+let noValue = null;
+
+console.log(typeof number)
+console.log(typeof string)
+console.log(typeof boolean)
+console.log(typeof undef)
+console.log(typeof noValue)
+
+
+let x = 1;
+let y = 3;
+let z = 9;
+console.log (x + y + z)
+console.log (x >= z)
+console.log (x <= y)
+console.log (x === y)
+
+console.log('Hello' + ' ' + 'World')
+
+let object = {   
+    nume: 'nume',
+    prenume: 'prenume',
+    hobby: 'hobby',
+    varsta: 'varsta',
+    profesie: 'profesie',
+}
+
+
+console.log(object)
+
+
+
